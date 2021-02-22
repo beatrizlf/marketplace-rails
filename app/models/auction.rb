4af68e6eabb3b327e66f17d6f907ecaf19450db6
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-02-22 17:35:09 -0300

    add validation for auction

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,6 @@
 class Auction < ApplicationRecord
+  CATEGORIES = ['Hot Toys', 'Mezco', 'Neca', 'Mafex', 'Iron Studios', 'Bandai']
+  has_many :bids
+  validates :name, presence: true 
+  validates :description, presence: true
 end
