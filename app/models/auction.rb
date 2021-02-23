484d75f9dbc778a523d5f205126f62756a0a81a8
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-02-23 13:04:08 -0300

    add validations to auction model

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,10 +1,13 @@
 class Auction < ApplicationRecord
   CATEGORIES = ['Hot Toys', 'Mezco', 'Neca', 'Mafex', 'Iron Studios', 'Bandai']
   has_many :bids
+  belongs_to :user
+  has_many_attached :photos
+
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :min_price, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :deadline, presence: true
-  validates :visible?, inclusion: [true, false]
+
 end
