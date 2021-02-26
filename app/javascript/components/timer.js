68167b04068824dbaa0bfd9d0c218b47c8ee43d9
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-02-25 22:02:34 -0300

    add some css some show card

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -22,7 +22,7 @@ const timerCountdown = () => {
               countdown = document.getElementById("countdown"),
               content = document.getElementById("content");
 
-          headline.innerText = "It's my birthday!";
+          headline.innerText = "Auction finished!";
           countdown.style.display = "none";
           content.style.display = "block";
 
